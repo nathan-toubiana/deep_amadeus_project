4f4527723bffc7a55fdb8e91414b94080587cb91
Nathan Toubiana <nlt2111@columbia.edu>
2017-11-28 17:34:15 -0500

    first commit

--- model.py
+++ model.py
@@ -5,3 +5,25 @@ from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
+
+
+class Model(object):
+    """
+    Class to define the architecture of the model
+    """
+    
+    
+
+    
+    
+    
+    
+    
+def training(X_train,Y_train):
+    """
+    training function
+    """
+
+
+
+
