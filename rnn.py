5deea76d3c07c374665b499f4893b0e24598584e
Nathan Toubiana <nlt2111@columbia.edu>
2017-11-28 14:35:26 -0500

    first push

---
+++ rnn.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import tensorflow as tf
+from tensorflow.contrib.rnn import RNNCell
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import math_ops
